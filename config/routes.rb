50e8b8208524b22110c4d445440fbda74b7dfe4f
dyoshida1352 <d.yoshida1352@gmail.com>
2021-06-19 15:35:26 +0000

    devise/controller/viewの実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admin', controllers: {
+    sessions:      'admin/sessions',
+    passwords:     'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+  devise_for :customers, path: 'customer', controllers: {
+    sessions:      'customer/sessions',
+    passwords:     'customer/passwords',
+    registrations: 'customer/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20210619151518_drop_table_customers.rb
@@ -0,0 +1,5 @@
+class DropTableCustomers < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :customers
+  end
+end
