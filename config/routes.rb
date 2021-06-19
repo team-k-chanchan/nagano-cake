61b0d6d99c27a251550eb6681c50e7d122ce6bb5
Horisuke96 <horisuke_52m@yahoo.co.jp>
2021-06-19 14:35:48 +0000

    [add]home-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :customer do
-    get 'homes/top'
-    get 'homes/about'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "customer/homes#top"
+  get "/about" => "customer/homes#about"
+
 end
