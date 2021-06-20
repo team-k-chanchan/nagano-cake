7fe91588f52165248c3ad3a3f21e80c5099e3b9f
yutaro313 <yuutarou55@icloud.com>
2021-06-20 05:08:18 +0000

    [add]admin-item

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  end
 end
