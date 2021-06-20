e2147da974cab9dc2eca4f4ae9bfac171745425c
tsukasatakahashi0614 <83329723+tsukasatakahashi0614@users.noreply.github.com>
2021-06-20 15:10:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get "/about" => "customer/homes#about"
   
   namespace :customer do
-      resources :items, only:[:index, :show, :new]
+      resources :items, only:[:index, :show]
   end
 
   namespace :admin do
