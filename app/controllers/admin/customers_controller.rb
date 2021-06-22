0ad8fedefaf84f0e106213f44560904d4ad8292c
dyoshida1352 <d.yoshida1352@gmail.com>
2021-06-22 13:44:03 +0000

    会員一覧、会員詳細/編集画面の作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
   def index
     @all_customer = Customer.all
     @customers = Customer.page(params[:page]).per(10)
@@ -13,6 +15,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+       redirect_to admin_customer_path(@customer)
+    else
+      render "edit"
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number, :email, :is_deleted)
   end
 
 end
