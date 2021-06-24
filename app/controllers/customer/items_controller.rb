58592c093aa877c5048d00344cb6e9ed78893997
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-24 04:54:47 +0000

    Item_Controller修正

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -2,7 +2,7 @@ class Customer::ItemsController < ApplicationController
 
 
     def index
-        @items = Item.where(sales_status: 0).page(params[:page]).per(8)
+        @items = Item.where(sales_status: true).page(params[:page]).per(8)
         @count = Item.count
     end
 
