21a325423800f6f3f57de37e97feb812418b840b
tsukasatakahashi0614 <tkspp2sko@gmail.com>
2021-06-25 06:58:24 +0000

    検索機能追加

--- app/controllers/customer/items_controller.rb
+++ app/controllers/customer/items_controller.rb
@@ -4,6 +4,7 @@ class Customer::ItemsController < ApplicationController
     def index
         @items = Item.where(sales_status: true).page(params[:page]).per(8)
         @count = Item.count
+        @genres = Genre.all
     end
 
     def show
