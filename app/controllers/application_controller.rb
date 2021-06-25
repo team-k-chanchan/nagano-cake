4043ca9de8c38e716ccd85557409a8e1a0b73cff
dyoshida1352 <d.yoshida1352@gmail.com>
2021-06-25 01:33:54 +0000

    [Fix]ログイン、ログアウト、新規登録後の遷移先変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後のページ遷移
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_orders_path
+    when Customer
+      root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
